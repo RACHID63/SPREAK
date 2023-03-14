c78ad4aa24a494faabd47906b80807e751196d5d
LAIEB Rachid <rachid.laieb@gmail.com>
2023-03-14 16:04:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function lire() {
+
+    // Récupérer le texte de l'élément textarea
+    var texte =
+    document.getElementById('texte').value;
+    // Créer un objet de synthèse vocale
+    var syntheseVocale = window.speechSynthesis;
+    var message = new SpeechSynthesisUtterance (texte);
+    // Sélectionner la voix
+    message.voiceURI = 'native';
+    message.lang 'fr_FR';
+    // Lire le message
+    syntheseVocale.speak (message);
+    
+    }
