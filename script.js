daa7adbe8d1c5e245eb342d41f60f7259be26e1b
LAIEB Rachid <rachid.laieb@gmail.com>
2023-03-23 14:48:37 +0100

    bouton lecture a mot

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-function lire() {
-
-    // Récupérer le texte de l'élément textarea
-    var texte =
-    document.getElementById('texte').value;
-    // Créer un objet de synthèse vocale
+function lire(texte) {
     var syntheseVocale = window.speechSynthesis;
-    var message = new SpeechSynthesisUtterance (texte);
-    // Sélectionner la voix
+    var message = new SpeechSynthesisUtterance(texte);
     message.voiceURI = 'native';
-    message.lang ='fr-FR';
-    // Lire le message
-    syntheseVocale.speak (message);
-    
+    message.lang = 'ar-AR';
+    syntheseVocale.speak(message);
+}
+
+function lireMot() {
+    var selectedWord = window.getSelection().toString();
+    if (selectedWord !== '') {
+        lire(selectedWord);
     }
+}
+
+function lireTout() {
+    var texte = document.getElementById('texte').value;
+    lire(texte);
+}
+
+function setupListeners() {
+    var readWordButton = document.getElementById('read-word');
+    readWordButton.addEventListener('mousedown', lireMot);
+    readWordButton.addEventListener('mouseup', () => window.getSelection().removeAllRanges());
+    document.getElementById('read-all').addEventListener('click', lireTout);
+}
+
+function handleTextChange() {
+    var words = document.getElementById('texte').value.split(' ');
+    var container = document.getElementById('text-container');
+    container.innerHTML = '';
+    words.forEach(function (word) {
+        var span = document.createElement('span');
+        span.className = 'word';
+        // span.innerText = word + ' ';
+        // container.appendChild(span);
+    });
+}
+
+document.getElementById('texte').addEventListener('input', handleTextChange);
+setupListeners();
